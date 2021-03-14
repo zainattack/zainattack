c639e15f92310cfe72f0182dc5584ece253d1304
zainattack <80650232+zainattack@users.noreply.github.com>
2021-03-15 00:49:11 +0300

    Create livel2.py

---
+++ livel2.py
@@ -0,0 +1,47 @@
+import os
+import time
+def cleare ():
+    os.system("clear")
+def numnum (isnumnum):
+    print ("✊" *isnumnum)
+
+def exit ():
+    os.system("exit")
+    os.system("exit")
+print (" \033[34m input numper >>>>")
+print (">>1<<")
+print (">>2<<")
+print ("<<3>>")
+print ("<<4>>")
+print ("<<5>>")
+print ("<<6>>")
+i = 1
+fackyou = int (input())
+if (fackyou ==  1 ) :
+
+    while i < 10 :
+
+        while i < 100 :
+            print ("\033[31m  HACK ")
+            i = i + 1
+elif (fackyou == 2) :
+    print ("\033[32m How many ??")
+    number = int(input())
+    print (" \033[31;1m fack..." * number)
+elif (fackyou == 3) :
+    print ("\033[32m How many ??")
+    isnum2 = int (input())
+    numnum (isnumnum = isnum2)
+
+elif (fackyou == 4):
+    time.sleep(1)
+    print ("😬" *1000)
+elif (fackyou == 5) :
+
+    os.system("cmatrix")
+elif (fackyou == 6):
+    cleare ()
+elif (fackyou == 7):
+    exit ()
+else:
+    print ("🖕")
